0fa7bd7d8d923ac30efb7f229c264bcff94ac752
adrise <adriesyounis@gmail.com>
2024-03-18 14:53:24 +0000

    add test coverage for left turn

--- tests/MartianRobotsUnitTests/DirectionTests.cs
+++ tests/MartianRobotsUnitTests/DirectionTests.cs
@@ -20,11 +20,29 @@ public class DirectionTests
     }
     
     [Test]
+    public void GivenNorthDirection_WhenTurningLeft_ThenDirectionIsWest()
+    {
+        this.Given(_ => ADirection(new North()))
+            .When(_ => TurningLeft())
+            .Then(_ => DirectionIs("W"))
+            .BDDfy();
+    }
+    
+    [Test]
     public void GivenEastDirection_WhenTurningRight_ThenDirectionIsSouth()
     {
         this.Given(_ => ADirection(new East()))
             .When(_ => TurningRight())
             .Then(_ => DirectionIs("S"))
+            .BDDfy();
+    }
+    
+    [Test]
+    public void GivenEastDirection_WhenTurningLeft_ThenDirectionIsNorth()
+    {
+        this.Given(_ => ADirection(new East()))
+            .When(_ => TurningLeft())
+            .Then(_ => DirectionIs("N"))
             .BDDfy();
     }
     
@@ -38,11 +56,29 @@ public class DirectionTests
     }
     
     [Test]
+    public void GivenSouthDirection_WhenTurningLeft_ThenDirectionIsEast()
+    {
+        this.Given(_ => ADirection(new South()))
+            .When(_ => TurningLeft())
+            .Then(_ => DirectionIs("E"))
+            .BDDfy();
+    }
+    
+    [Test]
     public void GivenWestDirection_WhenTurningRight_ThenDirectionIsNorth()
     {
         this.Given(_ => ADirection(new West()))
             .When(_ => TurningRight())
             .Then(_ => DirectionIs("N"))
+            .BDDfy();
+    }
+    
+    [Test]
+    public void GivenWestDirection_WhenTurningLeft_ThenDirectionIsSouth()
+    {
+        this.Given(_ => ADirection(new West()))
+            .When(_ => TurningLeft())
+            .Then(_ => DirectionIs("S"))
             .BDDfy();
     }
     
@@ -59,5 +95,10 @@ public class DirectionTests
     private void ADirection(IDirection direction)
     {
         _direction = direction;
+    }
+    
+    private void TurningLeft()
+    {
+        _expectedDirection =_direction.TurnLeft();
     }
 }
--- tests/MartianRobotsUnitTests/obj/Debug/net6.0/MartianRobotsUnitTests.AssemblyInfo.cs
+++ tests/MartianRobotsUnitTests/obj/Debug/net6.0/MartianRobotsUnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MartianRobotsUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e0512cdff0ef84885e9cff1d75974364188b63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b094a909a3a1a8ec3cadca80c7eedd64b861bd76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MartianRobotsUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MartianRobotsUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
